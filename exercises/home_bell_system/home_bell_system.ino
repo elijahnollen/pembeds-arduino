16e5d52c52ef45ac3aa974dae242fa92d1a11d28
Elijah Crisehea Nollen <206330144+elijahnollen@users.noreply.github.com>
2026-01-19 18:35:04 +0800

    Enhance home bell system with LED matrix and buzzer
    Refactor home bell system to include LED matrix and buzzer functionality. Added bell animations for different states.

--- exercises/home_bell_system/home_bell_system.ino
+++ exercises/home_bell_system/home_bell_system.ino
@@ -1,29 +1,99 @@
-#include <ArduinoIoTCloud.h>
-#include <Arduino_ConnectionHandler.h>
-#include <Arduino_NetworkConfigurator.h>
-#include "configuratorAgents/agents/BLEAgent.h"
-#include "configuratorAgents/agents/SerialAgent.h"
-void onBuzzerSwitchChange();
-void onLedChange();
+#include "thingProperties.h"
+#include "Arduino_LED_Matrix.h"
 
-bool buzzer_switch;
-bool led;
+const int BUZZER_PIN = 8; 
 
-KVStore kvStore;
-BLEAgentClass BLEAgent;
-SerialAgentClass SerialAgent;
-WiFiConnectionHandler ArduinoIoTPreferredConnection; 
-NetworkConfiguratorClass NetworkConfigurator(ArduinoIoTPreferredConnection);
+ArduinoLEDMatrix matrix;
 
-void initProperties(){
-  NetworkConfigurator.addAgent(BLEAgent);
-  NetworkConfigurator.addAgent(SerialAgent);
-  NetworkConfigurator.setStorage(kvStore);
-  // For changing the default reset pin uncomment and set your preferred pin. Use DISABLE_PIN for disabling the reset procedure.
-  //NetworkConfigurator.setReconfigurePin(your_pin);
-  ArduinoCloud.setConfigurator(NetworkConfigurator);
+// ORIGINAL CENTERED BELL
+uint8_t bell_frame[8][12] = {
+  { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+  { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
+  { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
 
-  ArduinoCloud.addProperty(buzzer_switch, READWRITE, ON_CHANGE, onBuzzerSwitchChange);
-  ArduinoCloud.addProperty(led, READWRITE, ON_CHANGE, onLedChange);
+// DING - SHIFTED RIGHT
+uint8_t bell_right[8][12] = {
+  { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
+  { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
 
+// DONG - SHIFTED LEFT
+uint8_t bell_left[8][12] = {
+  { 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+  { 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+void setup() {
+  Serial.begin(9600);
+  delay(1500); 
+
+  matrix.begin();
+
+  pinMode(13, OUTPUT);          
+  pinMode(BUZZER_PIN, OUTPUT); 
+
+  initProperties();
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();
+}
+
+void loop() {
+  ArduinoCloud.update();
+}
+
+void onLedChange()  {
+  if (led == true) {
+    digitalWrite(13, HIGH);
+    matrix.renderBitmap(bell_frame, 8, 12); 
+  } else {
+    digitalWrite(13, LOW);
+    matrix.clear(); 
+  }
+}
+
+void onBuzzerSwitchChange()  {
+  if (buzzer_switch == true) {
+    Serial.println("Ding Dong!");
+    
+    matrix.renderBitmap(bell_right, 8, 12);
+    tone(BUZZER_PIN, 2637); 
+    delay(300);            
+
+    matrix.renderBitmap(bell_left, 8, 12);
+    tone(BUZZER_PIN, 2093); 
+    delay(600);             
+
+    noTone(BUZZER_PIN);
+    
+    matrix.renderBitmap(bell_frame, 8, 12);
+    delay(400);
+    matrix.clear(); 
+    
+    buzzer_switch = false;
+    ArduinoCloud.update();
+    
+  } else {
+    noTone(BUZZER_PIN); 
+    matrix.clear();
+  }
 }
